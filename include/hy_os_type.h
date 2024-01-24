13f12de54434fad6965874a60e31a186dce961be
zhenquan.qiu <gnsyxiang@163.com>
2024-01-24 17:46:31 +0800

    feat: add hy_os_type.h file

---
+++ include/hy_os_type.h
@@ -0,0 +1,37 @@
+/**
+ *
+ * Release under GPLv-3.0.
+ * 
+ * @file    hy_os_type.h
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    24/01 2024 17:42
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        24/01 2024      create the file
+ * 
+ *     last modified: 24/01 2024 17:42
+ */
+#ifndef __LIBHY_OS_TYPE_INCLUDE_HY_OS_TYPE_H_
+#define __LIBHY_OS_TYPE_INCLUDE_HY_OS_TYPE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief 用于autotools编译脚本检测用
+ */
+void HyOsTypeCheck(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
---
+++ src/hy_os_type.c
@@ -0,0 +1,27 @@
+/**
+ * 
+ * Release under GPLv-3.0.
+ * 
+ * @file    hy_os_type.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    24/01 2024 17:44
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        24/01 2024      create the file
+ * 
+ *     last modified: 24/01 2024 17:44
+ */
+#include <stdio.h>
+
+#include "hy_os_type.h"
+
+void HyOsTypeCheck(void)
+{
+    printf("check libhy_os_type.so \n");
+}
